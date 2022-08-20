a42bb19a72f5ffb0f8f5cce26ffdba4831eddb77
Amankaium <kaium@list.ru>
2022-08-20 20:56:23 +0600

    lesson 2, add basket v2

--- src/App.js
+++ src/App.js
@@ -7,15 +7,34 @@ import BasketItem from './components/Basket/BasketItem';
 function App() {
   const [basket, setBasket] = useState([])
 
-  const addToBasket = (product) => {
-    setBasket([...basket, product])
+  const addToBasket = (product, newCount=0) => { // яблоко
+    const res = basket.find((element) => { return element.id === product.id})
+    if (!res) {
+      product.counter = 1
+      setBasket([...basket, product]) // [яблоко 1, груша 1]
+    } else {
+      const newBasket = basket.map((item) => {
+        if (item.id === res.id) {
+          if (newCount) {
+            item.counter = newCount
+          } else {
+            item.counter += 1
+          }
+          return item
+        }
+        return item
+      })
+
+      setBasket(newBasket)
+
+    }
   }
 
   return (
     <div className='App'>
       <ProductList addToBasket={addToBasket}/>
       <h2>Корзина</h2>
-      <div>{basket.map((product, index) => <BasketItem item={product} key={index}/>)}</div>
+      <div>{basket.map((product, index) => <BasketItem item={product} key={index} addToBasket={addToBasket}/>)}</div>
     </div>
   );
 }
--- src/components/Basket/BasketItem.js
+++ src/components/Basket/BasketItem.js
@@ -1,3 +1,26 @@
-export default function BasketItem({item}) {
-    return <div>{item.name}</div>
+import { useState, useEffect } from "react"
+
+export default function BasketItem({item, addToBasket}) {    
+    const style = {
+        inputStyle: {
+            width: '50px'
+        }
+    }
+
+    const changeCounter = (event) => {
+        const newValue = event.target.value
+        addToBasket(item, Number(newValue))
+    }
+
+    return (
+        <div>
+            {item.name}
+            <input
+                style={style.inputStyle}
+                type="number"
+                value={item.counter}
+                onChange={changeCounter}
+            />
+        </div>
+    )
 }
