571c2c3b953b1bdf9ae3f205955237d8baf7aa5a
Amankaium <kaium@list.ru>
2022-08-31 14:03:27 +0600

    start APIs

--- src/components/Product/ProductList.js
+++ src/components/Product/ProductList.js
@@ -1,17 +1,32 @@
 import ProductItem from "./ProductItem";
+import { useState, useEffect } from 'react';
 
 
 const ProductList = () => {
-    const products = [
-        {
-            id: 1,
-            name: "Яблоко"
-        },
-        {
-            id: 2,
-            name: "Груша"
-        },
-    ]
+    // const products = [
+    //     {
+    //         id: 1,
+    //         name: "Яблоко"
+    //     },
+    //     {
+    //         id: 2,
+    //         name: "Груша"
+    //     },
+    // ]
+
+    const [products, setProducts] = useState([])
+    
+    function getData() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(data => {
+                setProducts(data)
+                const date = new Date()
+                console.log(date)
+            })
+    }
+
+    useEffect(getData, [])
 
     return (
         <div>
