813c1f2a4711908d12cbb965cff67bd422dc5e96
Amankaium <kaium@list.ru>
2022-09-03 19:45:27 +0600

    add space in nav

--- src/components/Main/Nav.js
+++ src/components/Main/Nav.js
@@ -7,7 +7,7 @@ export default function Nav() {
         <nav>
           <div>
             <Link to="/">Главная</Link>&nbsp;&nbsp;&nbsp;
-            <Link to="/about">О нас</Link>
+            <Link to="/about">О нас</Link>&nbsp;&nbsp;&nbsp;
             <Link to="/contacts">Контакты</Link>
           </div>
           <BasketList/>
