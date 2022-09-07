f502818f9a6d772ad5eeb7f1b866bbf4c58560a8
Kebekbaeva <107349481+Kebekbaeva@users.noreply.github.com>
2022-09-07 19:20:32 +0600

    add input

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,14 @@
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+
 export default function LoginPage() {
     return (
-        <form>
-            <input type="text"/>
-            <input type="password"/>
-            <button>Войти</button>
-        </form>
+			<InputGroup className="mb-3">
+      <InputGroup.Text>First and last name</InputGroup.Text>
+      <Form.Control aria-label="First name" />
+      <Form.Control aria-label="Last name" />
+    </InputGroup>
+
     )
 }
