b17ce011558b10e2c5cfaedf62ff1aed5a853823
Kalimalieva <105165874+Kalimalieva@users.noreply.github.com>
2022-09-07 18:58:22 +0600

    lorem

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import LoginPage from './components/Auth/Login';
 import Contacts from './components/Contacts';
+import LoremPage from "./components/loremPage/LoremPage";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
               <Route path="/about" element={<About/>}/>
               <Route path="/login" element={<LoginPage/>}/>
               <Route path="/contacts" element={<Contacts/>}/>
+              <Route path="/lorem" element={<LoremPage/>}/>
           </Routes>
           
 
