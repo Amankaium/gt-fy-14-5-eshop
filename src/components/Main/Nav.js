ff60a132dbe06482ae0c3b2046e10c0cd3649b78
ganyevaman777 <ganyevaman7@gmail.com>
2022-09-14 18:28:58 +0600

    add styles

--- src/components/Main/Nav.js
+++ src/components/Main/Nav.js
@@ -1,18 +1,18 @@
 import { NavLink } from 'react-router-dom';
 import BasketList from '../Basket/BasketList';
-import './Nav.css'
+import classes from './Nav.module.css'
 
 
 export default function Nav() {
     return (
         <nav>
-          <div className='nav'> 
-              <NavLink className="link" to="/">Главная</NavLink>&nbsp;&nbsp;&nbsp;
-              <NavLink className="link" to="/about">О нас</NavLink>&nbsp;&nbsp;&nbsp;
-              <NavLink className="link" to="/login">Авторизация</NavLink>&nbsp;&nbsp;&nbsp;
-              <NavLink className="link" to="/contacts">Контакты</NavLink>&nbsp;&nbsp;&nbsp;
-              <NavLink className="link" to="/password">Пароль</NavLink>
-              <NavLink className="link" to="/lorem">Lorem</NavLink>
+          <div className={classes.nav}> 
+              <NavLink className={classes.link} to="/">Главная</NavLink>&nbsp;&nbsp;&nbsp;
+              <NavLink className={classes.link} to="/about">О нас</NavLink>&nbsp;&nbsp;&nbsp;
+              <NavLink className={classes.link} to="/login">Авторизация</NavLink>&nbsp;&nbsp;&nbsp;
+              <NavLink className={classes.link} to="/contacts">Контакты</NavLink>&nbsp;&nbsp;&nbsp;
+              <NavLink className={classes.link} to="/password">Пароль</NavLink>
+              <NavLink className={classes.link} to="/lorem">Lorem</NavLink>
           </div>
           <BasketList/>
         </nav>
